7ac7c08d921a84d67773c2ab01ef7ff7a260ca5e
activenoob <yusufkehinde11@gmail.com>
2020-06-10 23:31:04 +0100

    hotel webpage

---
+++ hotel.js
@@ -0,0 +1,15 @@
+console.log("hello");
+
+const navSlide = ()=>{
+   const menu = document.querySelector('.navbar__menu');
+   const nav = document.querySelector('.navbar__links');
+
+   menu.addEventListener('click', ()=>{
+     nav.classList.toggle('nav-active');
+
+   })
+
+}
+
+navSlide();
+
